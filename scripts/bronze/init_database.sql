be773bed4b832994e473fbccd7cb063b671bb35d
Kundanasai <121326714+Kundanasai@users.noreply.github.com>
2026-01-28 23:46:22 +0530

    Create init_database.sql

---
+++ scripts/bronze/init_database.sql
@@ -0,0 +1,30 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script purpose:  This script creates a new database named as 'DataWarehouse'.The script sets up three schemaswithin the database: 'bronze' ,'silver' ,'gold'
+*/
+
+USE master;
+
+--Drop and Recreate DataWarehouse
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name= 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END
+
+--create the 'datawarehouse' database
+CREATE DATABASE DataWarehouse;
+
+
+--use datawarehouse
+USE DataWarehouse;
+
+
+--create schemas
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
