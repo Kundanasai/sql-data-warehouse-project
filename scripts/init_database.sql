e168574e10f0f9effe6d79443f92cc5aa3ac75c0
Kundanasai <121326714+Kundanasai@users.noreply.github.com>
2026-01-28 05:57:33 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -8,6 +8,12 @@ Script purpose:  This script creates a new database named as 'DataWarehouse'.The
 
 USE master;
 
+--Drop and Recreate DataWarehouse
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name= 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END
 
 --create the 'datawarehouse' database
 CREATE DATABASE DataWarehouse;
